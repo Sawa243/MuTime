74535915a189e6ae071f9b62f9a9ae51c354964d
Alexandr <bichcov@mail.ru>
2022-09-03 14:15:24 +0300

    add new task  fix

--- app/src/main/java/com/sawacorp/mytime/view/NewTaskElement.kt
+++ app/src/main/java/com/sawacorp/mytime/view/NewTaskElement.kt
@@ -2,7 +2,11 @@ package com.sawacorp.mytime.view
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -11,11 +15,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.skydoves.colorpicker.compose.ColorEnvelope
 import com.github.skydoves.colorpicker.compose.HsvColorPicker
 import com.github.skydoves.colorpicker.compose.rememberColorPickerController
@@ -63,7 +68,7 @@ fun NewTaskElement(
                     .padding(top = 20.dp, bottom = 20.dp)
             ) {
                 Text(
-                    text = "Название Деятельности",
+                    text = "Добавить активность",
                     style = mainStyle
                 )
                 Spacer(
@@ -77,7 +82,7 @@ fun NewTaskElement(
                     modifier = Modifier.padding(start = 10.dp, end = 10.dp),
                     placeholder = { Text(text = "название") },
                     maxLines = 1,
-                    shape = RoundedCornerShape(25.dp),
+                    shape = RoundedCornerShape(15.dp),
                     singleLine = true,
                     textStyle = itemStyleText,
                     colors = TextFieldDefaults.outlinedTextFieldColors(textColor = Black90)
@@ -87,27 +92,49 @@ fun NewTaskElement(
                         .height(10.dp)
                         .fillMaxWidth()
                 )
-                Button(
-                    onClick = {
-                        focusManager.clearFocus()
-                        showColorPicker = !showColorPicker
-                    },
-                    shape = RoundedCornerShape(25.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                    contentPadding = PaddingValues(),
-                    modifier = Modifier.width(250.dp),
+                Text(
+                    text = "Цвет",
+                    modifier = Modifier
+                        .padding(start = 10.dp, end = 10.dp)
+                        .fillMaxWidth(),
+                    style = mainStyle,
+                    fontSize = 14.sp,
+                    color = Grey60,
+                    textAlign = TextAlign.Start
+                )
+                Spacer(
+                    Modifier
+                        .height(10.dp)
+                        .fillMaxWidth()
+                )
+                LazyRow(
+                    modifier = Modifier
+                        .padding(start = 10.dp, end = 10.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 ) {
-                    Box(
-                        modifier = Modifier
-                            .background(Brush.horizontalGradient(colors = listOf(Pink80, Cyan)))
-                            .padding(horizontal = 16.dp, vertical = 8.dp)
-                            .width(250.dp),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(
-                            text = "Выбрать цвет задачи",
-                            style = itemStylePeriod
+                    items(
+                        listOf(
+                            Color10,
+                            Color11,
+                            Color12,
+                            Color13,
+                            Color14,
                         )
+                    ) {
+                        Button(
+                            onClick = {
+                                focusManager.clearFocus()
+                                color = it
+                            },
+                            shape = CircleShape,
+                            colors = ButtonDefaults.buttonColors(containerColor = it),
+                            contentPadding = PaddingValues(),
+                            modifier = Modifier
+                                .padding(start = 5.dp, end = 5.dp)
+                                .size(40.dp),
+                        ) {}
                     }
                 }
                 Spacer(
@@ -115,30 +142,80 @@ fun NewTaskElement(
                         .height(10.dp)
                         .fillMaxWidth()
                 )
+                LazyRow(
+                    modifier = Modifier
+                        .padding(start = 10.dp, end = 10.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    items(
+                        listOf(
+                            Color15,
+                            Color16,
+                            Color17,
+                            Color18,
+                            Color19
+                        )
+                    ) {
+                        Button(
+                            onClick = {
+                                color = it
+                                focusManager.clearFocus()
+                            },
+                            shape = CircleShape,
+                            colors = ButtonDefaults.buttonColors(containerColor = it),
+                            contentPadding = PaddingValues(),
+                            modifier = Modifier
+                                .padding(start = 5.dp, end = 5.dp)
+                                .size(40.dp),
+                        ) {}
+                    }
+                }
+                Spacer(
+                    Modifier
+                        .height(20.dp)
+                        .fillMaxWidth()
+                )
                 Button(
                     onClick = { showPopUp(nameTask, color) },
-                    shape = RoundedCornerShape(25.dp),
+                    shape = RoundedCornerShape(15.dp),
                     colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
                     contentPadding = PaddingValues(),
                     modifier = Modifier.width(250.dp),
                 ) {
                     Box(
                         modifier = Modifier
-                            .background(Brush.horizontalGradient(colors = listOf(Color10, Color10)))
+                            .background(Brush.horizontalGradient(colors = listOf(Color10, Color19)))
                             .padding(horizontal = 16.dp, vertical = 8.dp)
                             .width(250.dp),
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
                             text = "Добавить задачу",
-                            style = itemStylePeriod
+                            style = itemStylePeriod,
+                            color = Black,
                         )
                     }
                 }
                 Spacer(
                     Modifier
-                        .height(10.dp)
+                        .height(5.dp)
                         .fillMaxWidth()
+                )
+                Text(
+                    text = "Выбрать другой цвет",
+                    modifier = Modifier
+                        .padding(start = 10.dp, end = 10.dp)
+                        .fillMaxWidth()
+                        .clickable {
+                            focusManager.clearFocus()
+                            showColorPicker = !showColorPicker
+                        },
+                    style = mainStyle,
+                    fontSize = 14.sp,
+                    color = Purple40,
+                    textAlign = TextAlign.Center
                 )
             }
         }
